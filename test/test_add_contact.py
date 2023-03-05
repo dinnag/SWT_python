71f85b28dc432e2ff631d5c4776d165fdb067ae9
dinna <dinna.gainullina@gmail.com>
2023-03-05 21:16:31 +0300

    tests: group creation and contact creation - change/ lesson 2.3 NavigationHelper, ContactHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def app(request):
     
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="wqew", middlename="hgfhg", lastname="kjhk", nickname="lklk", title=";kl;l", company=";lgffgfgf", address="hjgjhjk", home="khgkhj", mobile="khkhk",
+    app.contact.create_contact(Contact(firstname="wqew", middlename="hgfhg", lastname="kjhk", nickname="lklk", title=";kl;l", company=";lgffgfgf", address="hjgjhjk", home="khgkhj", mobile="khkhk",
                         work="dtgfhgj", fax="gfjfkjf", email="dudfj", email2="utudfdd", email3="jhjhgk", homepage="fhfgjgf", bday="2", bmonth="June", byear="1990", aday="5",
                         amonth="May", ayear="2000", address2="jhgjhj", phone2="jhjhkj", notes="kjkj"))
     app.session.logout()
