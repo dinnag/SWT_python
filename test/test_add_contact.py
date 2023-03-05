107e7f0e9d93ea096d59b4e4752a0414d3dc744e
dinna <dinna.gainullina@gmail.com>
2023-03-05 20:51:26 +0300

    tests: group creation and contact creation - change/ lesson 2.3 SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,9 +10,9 @@ def app(request):
     return fixture
     
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="wqew", middlename="hgfhg", lastname="kjhk", nickname="lklk", title=";kl;l", company=";lgffgfgf", address="hjgjhjk", home="khgkhj", mobile="khkhk",
                         work="dtgfhgj", fax="gfjfkjf", email="dudfj", email2="utudfdd", email3="jhjhgk", homepage="fhfgjgf", bday="2", bmonth="June", byear="1990", aday="5",
                         amonth="May", ayear="2000", address2="jhgjhj", phone2="jhjhkj", notes="kjkj"))
-    app.logout()
+    app.session.logout()
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,6 +10,6 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Test_1", header="Header_1", footer="Footer_1"))
-    app.logout()
+    app.session.logout()
