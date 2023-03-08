a0eb053bcfb51582d2ec97703e29c787101c20c4
dinna <dinna.gainullina@gmail.com>
2023-03-08 17:18:30 +0300

    test: delete first group

--- fixture/group.py
+++ fixture/group.py
@@ -26,7 +26,10 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        #submit deletion
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def open_groups_page(self):
             wd = self.app.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-    
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="wqew", middlename="hgfhg", lastname="kjhk", nickname="lklk", title=";kl;l", company=";lgffgfgf", address="hjgjhjk", homephone="khgkhj", mobilephone="khkhk",
