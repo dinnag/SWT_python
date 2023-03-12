33aa85c3dd10acf23d43c8c9dcd88d9cd5f168fa
dinna <dinna.gainullina@gmail.com>
2023-03-12 20:04:17 +0300

    add new test: first contact modification

--- fixture/group.py
+++ fixture/group.py
@@ -53,9 +53,9 @@ class GroupHelper:
         self.return_to_groups_page()
 
     def open_groups_page(self):
-            wd = self.app.wd
-            wd.find_element_by_link_text("groups").click()
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
 
     def return_to_groups_page(self):
-            wd = self.app.wd
-            wd.find_element_by_link_text("group page").click()
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
