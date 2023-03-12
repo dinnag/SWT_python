355e1ccf40b690c1be2679699f868b750674ef87
dinna <dinna.gainullina@gmail.com>
2023-03-12 21:36:49 +0300

    optimization: form filling

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-def test_modif_first_contact(app):
+def test_modify_first_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="test1", middlename="test2", lastname="kjhk", nickname="lklk", title=";kl;l", company=";lgffgfgf", address="hjgjhjk", homephone="khgkhj", mobilephone="khkhk",
                                              workphone="dtgfhgj", fax="gfjfkjf", email="dudfj", email2="utudfdd", email3="jhjhgk", homepage="fhfgjgf", bday="2", bmonth="June", byear="1990", aday="5",
                                              amonth="May", ayear="2000", address2="jhgjhj", phone2="jhjhkj", notes="kjkj"))
+    app.session.logout()
+
+def test_modify_first_contact_firstname(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(firstname="New_FirstName"))
     app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_modif_first_group(app):
+def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="Test_2", header="Header_2", footer="Footer_2"))
+    app.session.logout()
+
+def test_modify_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New_name"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New_header"))
     app.session.logout()
