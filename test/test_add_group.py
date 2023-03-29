db6d1e746138586092a113cb2d79ba68d91188bc
dinna <dinna.gainullina@gmail.com>
2023-03-29 19:32:00 +0300

    get_group_list

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,4 +2,7 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="Test_1", header="Header_1", footer="Footer_1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
